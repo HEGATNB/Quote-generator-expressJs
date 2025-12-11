a9131bdd513f0543f17962221a395079431c8163
HEGATNB <misharybakov2006@gmail.com>
2025-12-11 05:47:22 +0300

    тест

--- server.js
+++ server.js
@@ -12,9 +12,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// ВРЕМЕННО комментируем кастомные модули
-// const logger = require('./src/middleware/logger');
-// app.use(logger);
+const logger = require('./src/middleware/logger');
+app.use(logger);
 
 // const quotesRouter = require('./src/routes/quotes');
 // app.use('/api/quotes', quotesRouter);
