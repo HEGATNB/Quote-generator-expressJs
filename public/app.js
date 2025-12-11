37500cd5c0f388746d477c9bd44a6520857240ce
HEGATNB <149002661+HEGATNB@users.noreply.github.com>
2025-12-11 06:28:45 +0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://quote-generator-expressjs-1.onrender.com';
+const API_URL = 'https://quote-generator-expressjs-1.onrender.com/api/quotes';
 
 async function getRandomQuote() {
     try {
